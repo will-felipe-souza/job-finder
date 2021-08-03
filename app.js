fbbc4db9fd3574f82e7e087665091dcde10ac7c3
will-felipe-souza <will.felipe.souza@gmail.com>
2021-08-03 13:20:25 -0300

    Criado rota POST para inserir dados na tabela Jobs

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-const db = require('./db/connection')
+const db = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, function() {
   console.log(`O Express está rodando na porta ${PORT}`);
 });
+
+// body parser
+app.use(bodyParser.urlencoded({extended: false}));
 
 // db connection
 db
@@ -21,4 +25,7 @@ db
 // routes
 app.get('/', (req, res) => {
   res.send("Está funcionando 4");
-})
+});
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'));
