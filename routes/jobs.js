31bf5c08e492555c8ad5862b0e28b9570b37c805
will-felipe-souza <will.felipe.souza@gmail.com>
2021-08-10 19:46:50 -0300

    Criado HTML e rota da Vaga

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,12 +2,25 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
 
+
 // route add (form)
 router.get('/add', (req, res) => {
   res.render('add')
-})
+});
 
-// add job via post
+// rota detalhe da vaga
+router.get('/view/:id', (req, res) => {
+  Job.findOne({
+    where: {id: req.params.id}
+  })
+  .then(job => {
+    res.render('view', {
+      job
+    });
+  }).catch(err => console.log(err));
+});
+
+// rota add job via post (form)
 router.post('/add', (req, res) => {
   let {title, salary, company, description, email, new_job} = req.body;
 
